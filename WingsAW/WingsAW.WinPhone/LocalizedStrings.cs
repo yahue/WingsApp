bdf5d1815ccb8e997e3f79c7b678d5c900abe49c
arthur <yahui9119@163.com>
2015-12-19 20:44:29 +0800

    init

---
+++ WingsAW/WingsAW.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WingsAW.WinPhone.Resources;
+
+namespace WingsAW.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WingsAW/WingsAW.iOS/PhoneDialer.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using WingsAW.iOS;
+using System;
+using UIKit;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace WingsAW.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
